16ebce08ab66535582619dec87437ec5f230bd83
Alejandro Castor <128645676+BoemiaDEV@users.noreply.github.com>
2023-10-26 18:46:01 -0300

    Add files via upload

---
+++ scriptmain.js
@@ -0,0 +1,17 @@
+const tabMenu = document.querySelectorAll('.js-tabMenu li');
+const tabContent = document.querySelectorAll('.js-tabContent section');
+
+function activeTab(index){
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+    });
+});
+
+console.log(tabContent);
