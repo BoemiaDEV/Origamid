fc7b8798d0f2a5d9dbb9ff8b3d00cbf07213633d
BoemiaDEV <alejandrocastorfadim@gmail.com>
2023-11-03 10:18:32 -0300

    implermentação da função Accordion list no faq

--- scriptmain.js
+++ scriptmain.js
@@ -1,23 +1,43 @@
-const tabMenu = document.querySelectorAll('.js-tabMenu li');
-const tabContent = document.querySelectorAll('.js-tabContent section');
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabMenu li');
+    const tabContent = document.querySelectorAll('.js-tabContent section');
 
-if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
 
-    function activeTab(index){
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+        function activeTab(index){
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+        tabContent[index].classList.add('ativo');
+        };
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-    tabContent[index].classList.add('ativo');
-    };
+    }
+}
+initTabNav();
 
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
+function initAccordion(){
 
+    const accordion = document.querySelectorAll('.js-accordion dt');
+    const ativoClass = 'ativo';
+    if(accordion.length){
+        accordion[0].classList.add(ativoClass);
+        accordion[0].nextElementSibling.classList.add(ativoClass);
+        function activeAccordion() {
+            this.classList.toggle(ativoClass);
+            this.nextElementSibling.classList.toggle(ativoClass);
+        }
+
+        accordion.forEach((item) => {
+            item.addEventListener('mouseover', activeAccordion);
+
+        })  
+    }
 }
 
-console.log()
-
+initAccordion();
