bbdff209e64b2b824faf0bcb346680b2b90d36eb
BoemiaDEV <alejandrocastorfadim@gmail.com>
2023-11-03 13:32:18 -0300

    implermentação da funnção de scroll suave dos links do menu

--- scriptmain.js
+++ scriptmain.js
@@ -19,7 +19,7 @@ function initTabNav(){
         });
     }
 }
-initTabNav();
+
 
 function initAccordion(){
 
@@ -40,4 +40,35 @@ function initAccordion(){
     }
 }
 
+
+
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        const topo = section.offsetTop;
+
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    })
+
+}
+
+
+initTabNav();
 initAccordion();
+initScrollSuave();
