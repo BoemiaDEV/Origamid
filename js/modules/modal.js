8e122393de1e7f7bd6eaa87b15b2bcf793ceb400
BoemiaDEV <alejandrocastorfadim@gmail.com>
2023-11-25 13:07:13 -0300

    Implermentação do modal

---
+++ js/modules/modal.js
@@ -0,0 +1,27 @@
+export default  function initmodal(){
+
+
+
+}
+
+const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+const botaoFechar = document.querySelector('[data-modal="fechar"]');
+const containerModal = document.querySelector('[data-modal="container"]');
+
+if(botaoAbrir && botaoFechar && containerModal) {
+    function abrirModalAndFechar(event){
+        event.preventDefault()
+        containerModal.classList.toggle('ativo');
+    }
+    
+    function cliqueForadoModal(event){
+        if(event.target === this){
+            abrirModalAndFechar(event)
+        }
+    }
+
+
+    botaoAbrir.addEventListener('click', abrirModalAndFechar);
+    botaoFechar.addEventListener('click', abrirModalAndFechar);
+    containerModal.addEventListener('click', cliqueForadoModal)
+}
---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initmodal from './modules/modal.js'
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initmodal();
