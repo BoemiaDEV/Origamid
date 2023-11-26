4bd3cf5f5cb55a94e5fbdff21a5d98896f3cfb25
BoemiaDEV <alejandrocastorfadim@gmail.com>
2023-11-26 18:54:50 -0300

    implermentacao do menu drop Down]

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,21 @@
+import clickOutside from './outsideclick.js';
 export default function initDropdownMenu() {
+  const html = document.documentElement
 
+  const dropdownMenu = document.querySelectorAll('[data-dropdown]');
+
+
+  dropdownMenu.forEach((menu) => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    })
+  })
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add("ativo");
+    clickOutside(this, ['touchstart', 'click'], () => {
+      this.classList.remove('ativo')
+    })
+  }
 }
+
--- js/script.js
+++ js/script.js
@@ -13,3 +13,35 @@ initTabNav();
 initmodal();
 initTooltip();
 initDropdownMenu();
+
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const tempo = document.querySelector('.tempo');
+const guardatempo = document.querySelector('.guardatempo')
+iniciar.addEventListener('click', iniciarTempo);
+pausar.addEventListener('click', pausarTempo);
+tempo.addEventListener('click', resetarTempo);
+
+
+// let i = 0;
+
+// let timer;
+
+// function iniciarTempo(){
+//     timer = setInterval(() => {
+//         guardatempo.innerText = i++
+//     },100);
+//     iniciar.setAttribute('disabled', '');
+// }
+// function pausarTempo(){
+//     clearInterval(timer);
+//     iniciar.removeAttribute('disabled', '');
+// }
+// function resetarTempo(){
+//     guardatempo.innerText = 0;
+//     i = 0;
+//     iniciar.removeAttribute('disabled', '');
+// }
+
+
+// console.log('a')
