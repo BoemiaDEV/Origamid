d454027d10fc66d9d9ebdfbe9de280c4cd5e9773
BoemiaDEV <alejandrocastorfadim@gmail.com>
2023-11-29 06:14:42 -0300

    Implermentação do sistema de funcionamento

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initNumeros from './modules/numeros.js'
+import initFuncionamento from './modules/funcionamento.js';
 
 
 initScrollSuave();
@@ -18,6 +19,7 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initNumeros();
+initFuncionamento();
 // const iniciar = document.querySelector('.iniciar');
 // const pausar = document.querySelector('.pausar');
 // const tempo = document.querySelector('.tempo');
